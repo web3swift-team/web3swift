3b32224461f8510e743fa23bccbb437269f98525
Petr Korolev <sky4winder@gmail.com>
2017-12-06 02:53:53 +0300

    Initial Commit

---
+++ web3swift/web3swift.h
@@ -0,0 +1,19 @@
+//
+//  web3swift.h
+//  web3swift
+//
+//  Created by Petr Korolev on 06/12/2017.
+//  Copyright © 2017 Bankex Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for web3swift.
+FOUNDATION_EXPORT double web3swiftVersionNumber;
+
+//! Project version string for web3swift.
+FOUNDATION_EXPORT const unsigned char web3swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <web3swift/PublicHeader.h>
+
+
