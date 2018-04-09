389f18a70ac31025bf32c392ae788364fceed178
Alex Vlasov <av@bankexfoundation.org>
2018-04-09 19:32:29 +0300

    Fix sending unsigned transaction to external node Start the work on EIP67 QR encoder/decoder

--- web3swiftTests/web3swiftTests.swift
+++ web3swiftTests/web3swiftTests.swift
@@ -398,6 +398,23 @@ class web3swiftTests: XCTestCase {
         }
     }
     
+    func testEthSendExampleWithRemoveSigning() {
+        let web3 = Web3.new(URL.init(string: "http://127.0.0.1:8545")!)!
+        let sendToAddress = EthereumAddress("0x6394b37Cf80A7358b38068f0CA4760ad49983a1B")
+        let contract = web3.contract(Web3.Utils.coldWalletABI, at: sendToAddress, abiVersion: 2)
+        var options = Web3Options.defaultOptions()
+        options.value = Web3.Utils.parseToBigUInt("1.0", units: .eth)
+        options.from = EthereumAddress("0x804962017f0da9aa3970dc2adbA52A4c22614edB")
+        let intermediate = contract?.method("fallback", options: options)
+        guard let result = intermediate?.send(password: "") else {return XCTFail()}
+        switch result {
+        case .success(_):
+            return
+        case .failure(let error):
+            return XCTFail()
+        }
+    }
+    
     func testERC20Encode() {
         let jsonString = "[{\"constant\":true,\"inputs\":[],\"name\":\"name\",\"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_spender\",\"type\":\"address\"},{\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"approve\",\"outputs\":[{\"name\":\"success\",\"type\":\"bool\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"totalSupply\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_from\",\"type\":\"address\"},{\"name\":\"_to\",\"type\":\"address\"},{\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"transferFrom\",\"outputs\":[{\"name\":\"success\",\"type\":\"bool\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"decimals\",\"outputs\":[{\"name\":\"\",\"type\":\"uint8\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"version\",\"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"_owner\",\"type\":\"address\"}],\"name\":\"balanceOf\",\"outputs\":[{\"name\":\"balance\",\"type\":\"uint256\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"symbol\",\"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_to\",\"type\":\"address\"},{\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"transfer\",\"outputs\":[{\"name\":\"success\",\"type\":\"bool\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_spender\",\"type\":\"address\"},{\"name\":\"_value\",\"type\":\"uint256\"},{\"name\":\"_extraData\",\"type\":\"bytes\"}],\"name\":\"approveAndCall\",\"outputs\":[{\"name\":\"success\",\"type\":\"bool\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"_owner\",\"type\":\"address\"},{\"name\":\"_spender\",\"type\":\"address\"}],\"name\":\"allowance\",\"outputs\":[{\"name\":\"remaining\",\"type\":\"uint256\"}],\"payable\":false,\"type\":\"function\"},{\"inputs\":[{\"name\":\"_initialAmount\",\"type\":\"uint256\"},{\"name\":\"_tokenName\",\"type\":\"string\"},{\"name\":\"_decimalUnits\",\"type\":\"uint8\"},{\"name\":\"_tokenSymbol\",\"type\":\"string\"}],\"type\":\"constructor\"},{\"payable\":false,\"type\":\"fallback\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"name\":\"_from\",\"type\":\"address\"},{\"indexed\":true,\"name\":\"_to\",\"type\":\"address\"},{\"indexed\":false,\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"Transfer\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"name\":\"_owner\",\"type\":\"address\"},{\"indexed\":true,\"name\":\"_spender\",\"type\":\"address\"},{\"indexed\":false,\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"Approval\",\"type\":\"event\"},]"
         do {
@@ -1787,7 +1804,35 @@ class web3swiftTests: XCTestCase {
         }
     }
     
-   
+    func testEIP67encoding() {
+        var eip67Data = Web3.Utils.EIP67Code.init(address: EthereumAddress("0x6394b37Cf80A7358b38068f0CA4760ad49983a1B"))
+        eip67Data.gasLimit = BigUInt(21000)
+        eip67Data.amount = BigUInt("1000000000000000000")
+//        eip67Data.data =
+        let encoding = eip67Data.toString()
+        print(encoding)
+    }
+    
+    func testEIP67codeGeneration() {
+        var eip67Data = Web3.Utils.EIP67Code.init(address: EthereumAddress("0x6394b37Cf80A7358b38068f0CA4760ad49983a1B"))
+        eip67Data.gasLimit = BigUInt(21000)
+        eip67Data.amount = BigUInt("1000000000000000000")
+        //        eip67Data.data =
+        let encoding = eip67Data.toImage(scale: 5.0)
+        XCTAssert(encoding != CIImage())
+    }
+    
+    func testEIP67decoding() {
+        var eip67Data = Web3.Utils.EIP67Code.init(address: EthereumAddress("0x6394b37Cf80A7358b38068f0CA4760ad49983a1B"))
+        eip67Data.gasLimit = BigUInt(21000)
+        eip67Data.amount = BigUInt("1000000000000000000")
+        //        eip67Data.data =
+        let encoding = eip67Data.toString()
+        guard let code = Web3.Utils.EIP67CodeParser.parse(encoding) else {return XCTFail()}
+        XCTAssert(code.address == eip67Data.address)
+        XCTAssert(code.gasLimit == eip67Data.gasLimit)
+        XCTAssert(code.amount == eip67Data.amount)
+    }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
