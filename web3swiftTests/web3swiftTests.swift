6b672fc0526dc0766e5e5b160833fb06920d7946
Petr Korolev <sky4winder@gmail.com>
2018-04-16 23:30:04 +0200

    small fixes

--- web3swiftTests/web3swiftTests.swift
+++ web3swiftTests/web3swiftTests.swift
@@ -962,8 +962,9 @@ class web3swiftTests: XCTestCase {
                                        values: [number!] as [AnyObject])
         XCTAssert(data != nil, "failed to encode")
         let expected = "0xfffffffffffff38dd0f10627f5529bdb2c52d4846810af0ac000000000000001"
-        print(data?.toHexString().lowercased().addHexPrefix())
-        XCTAssert(data?.toHexString().lowercased().addHexPrefix() == expected, "failed to encode")
+        let result = data?.toHexString().lowercased().addHexPrefix()
+        print(result)
+        XCTAssert(result == expected, "failed to encode")
     }
 
     func testABIv2encoding5()
