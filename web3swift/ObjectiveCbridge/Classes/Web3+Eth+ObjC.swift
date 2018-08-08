a702fdb769b0738f4f1efe956dfb35f2533f7544
Alex Vlasov <av@bankexfoundation.org>
2018-08-08 12:53:19 +0300

    start an initial work on ObjC bridging

---
+++ web3swift/ObjectiveCbridge/Classes/Web3+Eth+ObjC.swift
@@ -0,0 +1,37 @@
+//
+//  Web3+Eth+ObjC.swift
+//  web3swift
+//
+//  Created by Alexander Vlasov on 08.08.2018.
+//  Copyright © 2018 Bankex Foundation. All rights reserved.
+//
+
+import Foundation
+
+@objc(web3Eth)
+public final class _ObjCweb3Eth: NSObject {
+    private (set) weak var web3: web3?
+    
+    init(web3: web3?) {
+        self.web3 = web3
+    }
+    
+    public func getBalance(address: _ObjCEthereumAddress, onBlock: NSString = "latest", error: NSErrorPointer) -> _ObjCBigUInt? {
+        guard let addr = address.address else {
+            error?.pointee = Web3Error.inputError("Address is empty") as NSError
+            return nil
+        }
+        guard let result = self.web3?.eth.getBalance(address: addr, onBlock: onBlock as String) else {
+            error?.pointee = Web3Error.processingError("Web3 object was not properly initialized") as NSError
+            return nil
+        }
+        switch result {
+        case .success(let balance):
+            let biguint = _ObjCBigUInt(value: balance)
+            return biguint
+        case .failure(let web3error):
+            error?.pointee = web3error as NSError
+            return nil
+        }
+    }
+}
