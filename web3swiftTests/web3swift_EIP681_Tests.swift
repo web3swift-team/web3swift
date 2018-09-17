99b076ba0f161fc67af4e09ccafb47f526735a54
Georgii Fesenko <fesenko.g@gmail.com>
2018-09-17 17:35:38 +0300

    Basic ENS functins added, EIP681 parsing now can parse ENS

--- web3swiftTests/web3swift_EIP681_Tests.swift
+++ web3swiftTests/web3swift_EIP681_Tests.swift
@@ -19,12 +19,17 @@ class web3swift_EIP681_Tests: XCTestCase {
     //ethereum:0x45245bc59219eeaaf6cd3f382e078a461ff9de7b/transfer?address=0x6891dC3962e710f0ff711B9c6acc26133Fd35Cb4&uint256=1
     
     func testEIP681Parsing() {
-        let parsed = Web3.EIP681CodeParser.parse("ethereum:0xfb6916095ca1df60bb79Ce92ce3ea74c37c5d359?value=2.014e18")
+        let parsed = Web3.EIP681CodeParser.parse("ethereum:0x5ffc014343cd971b7eb70732021e26c35b744cc4?value=2.014e18")
         XCTAssert(parsed != nil)
     }
     
     func testEIP681Parsing2() {
         let parsed = Web3.EIP681CodeParser.parse("ethereum:0x45245bc59219eeaaf6cd3f382e078a461ff9de7b/transfer?address=0x6891dC3962e710f0ff711B9c6acc26133Fd35Cb4&uint256=1")
+        XCTAssert(parsed != nil)
+    }
+    
+    func testEIP681ENSParsing() {
+        let parsed = Web3.EIP681CodeParser.parse("ethereum:somename.eth/transfer?address=somename.eth&uint256=1")
         XCTAssert(parsed != nil)
     }
     
--- web3swiftTests/web3swift_ENS_Tests.swift
+++ web3swiftTests/web3swift_ENS_Tests.swift
@@ -21,4 +21,55 @@ class web3swift_ENS_Tests: XCTestCase {
         XCTAssertEqual(NameHash.nameHash("foo.eth"), Data.fromHex("0xde9b09fd7c5f901e23a3f19fecc54828e9c848539801e86591bd9801b019f84f"))
     }
     
+    func testResolverAddress() {
+        let web = web3(provider: InfuraProvider(Networks.Mainnet)!)
+        var ens = ENS(web3: web)
+        guard case .success(let resolver) = ens.resolver(forDomain: "somename.eth") else { XCTAssert(false); return }
+        XCTAssertEqual(resolver.resolverAddress.address.lowercased(), "0x5ffc014343cd971b7eb70732021e26c35b744cc4")
+    }
+    
+    func testResolver() {
+        let web = web3(provider: InfuraProvider(Networks.Mainnet)!)
+        var ens = ENS(web3: web)
+        let domain = "somename.eth"
+        guard case .success(var resolver) = ens.resolver(forDomain: domain) else { XCTAssert(false); return  }
+        guard case .success(let address) = resolver.addr(forDomain: domain) else { XCTAssert(false); return  }
+        XCTAssertEqual(address.address.lowercased(), "0x3487acfb1479ad1df6c0eb56ae743d34897798ac")
+        
+    }
+    
+    func testSupportsInterface() {
+        let web = web3(provider: InfuraProvider(Networks.Mainnet)!)
+        var ens = ENS(web3: web)
+        let domain = "somename.eth"
+        guard case .success(var resolver) = ens.resolver(forDomain: domain) else { XCTAssert(false); return  }
+        guard case .success(let isAddrSupports) = resolver.supportsInterface(interfaceID: ResolverENS.InterfaceName.addr.hash()) else { XCTAssert(false); return  }
+        XCTAssertEqual(isAddrSupports, true)
+        guard case .success(let isNameSupports) = resolver.supportsInterface(interfaceID: ResolverENS.InterfaceName.name.hash()) else { XCTAssert(false); return  }
+        XCTAssertEqual(isNameSupports, true)
+        guard case .success(let isABIsupports) = resolver.supportsInterface(interfaceID: ResolverENS.InterfaceName.ABI.hash()) else { XCTAssert(false); return  }
+        XCTAssertEqual(isABIsupports, true)
+        guard case .success(let isPubkeySupports) = resolver.supportsInterface(interfaceID: ResolverENS.InterfaceName.pubkey.hash()) else { XCTAssert(false); return  }
+        XCTAssertEqual(isPubkeySupports, true)
+    }
+    
+    func testABI() {
+        let web = web3(provider: InfuraProvider(Networks.Mainnet)!)
+        var ens = ENS(web3: web)
+        let domain = "somename.eth"
+        guard case .success(var resolver) = ens.resolver(forDomain: domain) else { XCTAssert(false); return }
+        guard case .success(let isABIsupported) = resolver.supportsInterface(interfaceID: ResolverENS.InterfaceName.ABI.hash()) else { XCTAssert(false); return }
+        if isABIsupported {
+            guard case .success(let res) = resolver.ABI(node: domain, contentType: 2) else { XCTAssert(false); return }
+            XCTAssert(res.0 == 0)
+            XCTAssert(res.1.count == 0)
+        }
+    }
+    
+    func testName() {
+        //TODO
+    }
+    
+    
+    
 }
