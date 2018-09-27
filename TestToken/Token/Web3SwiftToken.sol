29235a801ca5e7ee48211432f49e651c4b5c7fcf
Anton Grigoriev <antongrigorjev2010@gmail.com>
2018-09-27 18:39:50 +0300

    Update Web3SwiftToken.sol

--- TestToken/Token/Web3SwiftToken.sol
+++ TestToken/Token/Web3SwiftToken.sol
@@ -2,13 +2,13 @@ pragma solidity ^0.4.24;
 
 import "../Helpers/TokenBasics/ERC20.sol";
 
-contract Web3SwiftToken is ERC20 {
+contract web3swift is ERC20 {
 
-  string public constant name = "Web3Swift";
-  string public constant symbol = "W3S";
+  string public constant name = "web3swift";
+  string public constant symbol = "w3s";
   uint8 public constant decimals = 18;
 
-  uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
+  uint256 public constant INITIAL_SUPPLY = 1024 * (10 ** uint256(decimals));
 
   /**
    * @dev Constructor that gives msg.sender all of existing tokens.
