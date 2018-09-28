60634b5142332232f6fef038a5a5abe1c679d6cf
Anton Grigoriev <antongrigorjev2010@gmail.com>
2018-09-28 15:42:56 +0300

    Update web3swift_ERC20_Class_Tests.swift

--- web3swiftTests/web3swift_ERC20_Class_Tests.swift
+++ web3swiftTests/web3swift_ERC20_Class_Tests.swift
@@ -17,22 +17,22 @@ class web3swift_ERC20_Class_Tests: XCTestCase {
     
     func testERC20TokenCreation() {
         let web3 = Web3.InfuraMainnetWeb3()
-        let SALTtokenAddress = EthereumAddress("0x4156D3342D5c385a87D264F90653733592000581")!
-        let erc20token = ERC20.init(web3: web3, provider: web3.provider, address: SALTtokenAddress)
+        let w3sTokenAddress = EthereumAddress("0x8932404A197D84Ec3Ea55971AADE11cdA1dddff1")!
+        let erc20token = ERC20.init(web3: web3, provider: web3.provider, address: w3sTokenAddress)
         erc20token.readProperties()
-        XCTAssert(erc20token.symbol == "SALT")
-        XCTAssert(erc20token.name == "Salt")
-        XCTAssert(erc20token.decimals == 8)
+        XCTAssert(erc20token.symbol == "w3s")
+        XCTAssert(erc20token.name == "web3swift")
+        XCTAssert(erc20token.decimals == 18)
     }
     
     func testERC20tokenBalanceAndAllowance() {
         let web3 = Web3.InfuraMainnetWeb3()
-        let SALTtokenAddress = EthereumAddress("0x4156D3342D5c385a87D264F90653733592000581")!
-        let erc20token = ERC20.init(web3: web3, provider: web3.provider, address: SALTtokenAddress)
+        let w3sTokenAddress = EthereumAddress("0x8932404A197D84Ec3Ea55971AADE11cdA1dddff1")!
+        let erc20token = ERC20.init(web3: web3, provider: web3.provider, address: w3sTokenAddress)
         let userAddress = EthereumAddress("0xe22b8979739D724343bd002F9f432F5990879901")!
         guard case .success(let balance) = erc20token.getBalance(account: userAddress) else {return XCTFail()}
         guard case .success(let allowance) = erc20token.getAllowance(originalOwner: userAddress, delegate: userAddress) else {return XCTFail()}
-        XCTAssert(String(balance) == "0")
+        XCTAssert(String(balance) == "1024000000000000000000")
         XCTAssert(allowance == 0)
     }
 }
