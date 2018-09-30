e096f71eb93e25eadb63b3677c3a3df8ec71507b
Petr Korolev <sky4winder@gmail.com>
2018-09-30 15:46:44 +0300

    update pod in main repo

--- Example/web3swiftExample/Pods/Headers/Public/PromiseKit/PromiseKit-umbrella.h
+++
@@ -1,1 +0,0 @@
-/Users/petrkorolev/repo/web3swift/Example/web3swiftExample/Pods/Target Support Files/PromiseKit/PromiseKit-umbrella.h
--- Example/web3swiftExample/Pods/Headers/Public/web3swift/web3swift-umbrella.h
+++
@@ -1,1 +0,0 @@
-/Users/petrkorolev/repo/web3swift/Example/web3swiftExample/Pods/Target Support Files/web3swift/web3swift-umbrella.h
