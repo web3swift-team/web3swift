97dfecea73e5044be3de9a250d171d3d8a4fe0c9
Alex Vlasov <alex.m.vlasov@gmail.com>
2018-10-04 17:10:33 +0300

    fix fallback to scientific in formatting

--- web3swift/Web3/Classes/Web3+Utils.swift
+++ web3swift/Web3/Classes/Web3+Utils.swift
@@ -675,8 +675,8 @@ extension Web3.Utils {
         return formatToPrecision(bigNumber, numberDecimals: toUnits.decimals, formattingDecimals: decimals, decimalSeparator: decimalSeparator, fallbackToScientific: fallbackToScientific);
     }
     
-    /// Formats a BigUInt object to String. The supplied number is first divided into integer and decimal part based on "toUnits",
-    /// then limit the decimal part to "decimals" symbols and uses a "decimalSeparator" as a separator.
+    /// Formats a BigUInt object to String. The supplied number is first divided into integer and decimal part based on "numberDecimals",
+    /// then limits the decimal part to "formattingDecimals" symbols and uses a "decimalSeparator" as a separator.
     /// Fallbacks to scientific format if higher precision is required.
     ///
     /// Returns nil of formatting is not possible to satisfy.
@@ -691,7 +691,7 @@ extension Web3.Utils {
         }
         let divisor = BigUInt(10).power(unitDecimals)
         let (quotient, remainder) = bigNumber.quotientAndRemainder(dividingBy: divisor)
-        let fullRemainder = String(remainder);
+        var fullRemainder = String(remainder);
         let fullPaddedRemainder = fullRemainder.leftPadding(toLength: unitDecimals, withPad: "0")
         let remainderPadded = fullPaddedRemainder[0..<toDecimals]
         if remainderPadded == String(repeating: "0", count: toDecimals) {
@@ -703,6 +703,18 @@ extension Web3.Utils {
                     if (char == "0") {
                         firstDigit = firstDigit + 1;
                     } else {
+                        let firstDecimalUnit = String(fullPaddedRemainder[firstDigit ..< firstDigit+1])
+                        var remainingDigits = ""
+                        let numOfRemainingDecimals = fullPaddedRemainder.count - firstDigit - 1
+                        if numOfRemainingDecimals <= 0 {
+                            remainingDigits = ""
+                        } else if numOfRemainingDecimals > formattingDecimals {
+                            let end = firstDigit+1+formattingDecimals > fullPaddedRemainder.count ? fullPaddedRemainder.count : firstDigit+1+formattingDecimals
+                            remainingDigits = String(fullPaddedRemainder[firstDigit+1 ..< end])
+                        } else {
+                            remainingDigits = String(fullPaddedRemainder[firstDigit+1 ..< fullPaddedRemainder.count])
+                        }
+                        fullRemainder = firstDecimalUnit + decimalSeparator + remainingDigits
                         firstDigit = firstDigit + 1;
                         break
                     }
--- web3swiftTests/web3swift_numberFormattingUtil_Tests.swift
+++ web3swiftTests/web3swift_numberFormattingUtil_Tests.swift
@@ -57,4 +57,16 @@ class web3swift_numberFormattingUtil_Tests: XCTestCase {
         XCTAssert(formatted == "-1,1000")
     }
     
+    func testNumberFormattingUtil8() {
+        let balance = BigInt("100")!
+        let formatted = Web3.Utils.formatToPrecision(balance, numberDecimals: 18, formattingDecimals: 4, decimalSeparator: ",", fallbackToScientific: true)
+        XCTAssert(formatted == "1,00e-16")
+    }
+    
+    func testNumberFormattingUtil9() {
+        let balance = BigInt("1000000")!
+        let formatted = Web3.Utils.formatToPrecision(balance, numberDecimals: 18, formattingDecimals: 4, decimalSeparator: ",", fallbackToScientific: true)
+        XCTAssert(formatted == "1,0000e-12")
+    }
+    
 }
