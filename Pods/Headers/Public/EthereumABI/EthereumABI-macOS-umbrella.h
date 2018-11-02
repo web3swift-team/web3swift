b988fa537d6a2f9f707a659f252db4c485ca8201
BaldyAsh <antongrigorjev2010@gmail.com>
2018-11-02 16:09:09 +0300

    updated pods to fix building

--- Pods/Headers/Public/EthereumABI/EthereumABI-macOS-umbrella.h
+++ Pods/Headers/Public/EthereumABI/EthereumABI-macOS-umbrella.h
@@ -1,1 +1,1 @@
-/Users/alexvlasov/Blockchain/web3swift/web3swift/Pods/Target Support Files/EthereumABI-macOS/EthereumABI-macOS-umbrella.h
+/Users/antongrigorev/Developer/web3swift/Pods/Target Support Files/EthereumABI-macOS/EthereumABI-macOS-umbrella.h
