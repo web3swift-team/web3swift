68ed43c2b69d29d885eb75e35b4587c4fc5f9acf
BaldyAsh <antongrigorjev2010@gmail.com>
2018-11-20 15:51:59 +0300

    test fix

--- Pods/Headers/Public/SipHash/SipHash-macOS-umbrella.h
+++ Pods/Headers/Public/SipHash/SipHash-macOS-umbrella.h
@@ -1,1 +1,1 @@
-/Users/alexvlasov/Blockchain/web3swift/web3swift/Pods/Target Support Files/SipHash-macOS/SipHash-macOS-umbrella.h
+/Users/antongrigorev/Developer/web3swift/Pods/Target Support Files/SipHash-macOS/SipHash-macOS-umbrella.h
