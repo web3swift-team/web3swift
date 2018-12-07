2dd4ca05b4bdf5619f19874d6f5dbc4ae628505b
BaldyAsh <antongrigorjev2010@gmail.com>
2018-12-07 03:03:37 +0300

    fixed eip681 and eip67

--- Pods/Headers/Public/BigInt/BigInt-macOS-umbrella.h
+++ Pods/Headers/Public/BigInt/BigInt-macOS-umbrella.h
@@ -1,1 +1,1 @@
-/Users/antongrigorev/Developer/web3swift/Pods/Target Support Files/BigInt-macOS/BigInt-macOS-umbrella.h
+/Users/baldyash/Developer/web3swift/Pods/Target Support Files/BigInt-macOS/BigInt-macOS-umbrella.h
