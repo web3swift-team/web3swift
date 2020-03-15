2240471f8e48247dcebd283d788c35c30dfbea2e
BaldyAsh <antongrigorjev2010@gmail.com>
2020-03-16 00:57:31 +0300

    example browser not work

---
+++ Example/web3swiftBrowser/Pods/Target Support Files/Pods-web3swiftBrowser/Pods-web3swiftBrowser-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_web3swiftBrowser : NSObject
+@end
+@implementation PodsDummy_Pods_web3swiftBrowser
+@end
---
+++ Example/web3swiftBrowser/Pods/Target Support Files/WKBridge/WKBridge-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WKBridge : NSObject
+@end
+@implementation PodsDummy_WKBridge
+@end
