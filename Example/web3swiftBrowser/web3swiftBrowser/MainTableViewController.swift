0a8d7f56339be624ae0c408e88d91653a38b8449
Petr Korolev <sky4winder@gmail.com>
2020-10-26 19:28:14 +0700

    update pods, add table view for test functions

---
+++ Example/web3swiftBrowser/web3swiftBrowser/MainTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainTableViewController.swift
+//  web3swiftBrowser
+//
+//  Created by Petr Korolev on 26.10.2020.
+//  Copyright © 2020 Matter Labs. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+ 
+
+}
