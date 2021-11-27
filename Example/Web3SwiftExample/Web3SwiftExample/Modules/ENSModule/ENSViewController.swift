0231502386e4dd68f3d3b66f70e312330a782c92
Maksim Alekseev <mrvalter566@gmail.com>
2021-11-27 20:37:38 +0700

    Add ens and network module

---
+++ Example/Web3SwiftExample/Web3SwiftExample/Modules/ENSModule/ENSViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ENSViewController.swift
+//  Web3SwiftExample
+//
+//  Created by Максим Алексеев  on 26.11.2021.
+//
+
+import UIKit
+
+class ENSViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Example/Web3SwiftExample/Web3SwiftExample/Modules/NetworkModule/NetworkViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NetworkViewController.swift
+//  Web3SwiftExample
+//
+//  Created by Максим Алексеев  on 05.11.2021.
+//
+
+import UIKit
+
+class NetworkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
