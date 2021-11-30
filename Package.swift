14e270b1aa9affcecf4465c0614327b384da60e4
Yaroslav <yaroslav.yashin@me.com>
2021-11-30 14:28:21 +0300

    First attempt of github actions.

--- Package.swift
+++ Package.swift
@@ -15,21 +15,20 @@ let excludeFiles: String = []
 let package = Package(
     name: "Web3swift",
     platforms: [
-        .macOS(.v10_12), .iOS(.v11),
+        .macOS(.v10_12), .iOS(.v11)
     ],
     products: [
-        .library(name: "web3swift", targets: ["web3swift"]),
+        .library(name: "web3swift", targets: ["web3swift"])
     ],
     
     dependencies: [
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
         .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.15.4"),
         .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.4"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.2"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.2")
     ],
     targets: [
         .target(name: "secp256k1"),
-
         .target(
             name: "web3swift",
             dependencies: ["BigInt", "secp256k1", "PromiseKit", "Starscream", "CryptoSwift"],
@@ -42,6 +41,13 @@ let package = Package(
         ),
         .testTarget(
             name: "web3swiftTests",
-            dependencies: ["web3swift"]),
+            dependencies: ["web3swift"],
+            resources: [
+                .copy("../TestToken/Helpers/SafeMath/SafeMath.sol"),
+                .copy("../TestToken/Helpers/TokenBasics/ERC20.sol"),
+                .copy("../TestToken/Helpers/TokenBasics/IERC20.sol"),
+                .copy("../TestToken/Token/Web3SwiftToken.sol")
+            ]
+        ),
     ]
 )
