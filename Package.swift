858e92cb38d5310c74bd595436a6757f3f0b6998
Mark Loit <mloit@mac.com>
2022-03-15 13:38:26 -0400

    Undo unintentional changes to Package.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 
-#if os(iOS)
-let excludeFiles: String = []
-#else
+#if os(macOS)
 let excludeFiles = [
     "./Browser/BrowserViewController.swift", // Because of inheriting iOS only class failed to build on macOS.
 ]
+#elseif os(iOS)
+let excludeFiles: String = []
 #endif
 
 let package = Package(
