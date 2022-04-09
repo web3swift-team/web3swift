eaf61898706f9fe4be3ad91479c4aff56e5bef6a
Yaroslav <yaroslav.yashin@me.com>
2022-04-09 14:47:01 +0300

    Refactoring
    - Rename DecodableFromHex init to make it more explicit.
    - Rename `slf` to `type` local variable in `getValue<T>() -> T?`.
    - Fix bug with pre EIP-1559 Block decoding support.

--- Sources/web3swift/Web3/Web3+Structures.swift
+++ Sources/web3swift/Web3/Web3+Structures.swift
@@ -339,7 +339,7 @@ public struct Block: Decodable {
     public var size: BigUInt
     public var gasLimit: BigUInt
     public var gasUsed: BigUInt
-    public var baseFeePerGas: BigUInt
+    public var baseFeePerGas: BigUInt?
     public var timestamp: Date
     public var transactions: [TransactionInBlock]
     public var uncles: [Data]
@@ -401,7 +401,9 @@ extension Block {
         self.size = try container.decodeHex(to: BigUInt.self, key: .size)
         self.gasLimit = try container.decodeHex(to: BigUInt.self, key: .gasLimit)
         self.gasUsed = try container.decodeHex(to: BigUInt.self, key: .gasUsed)
-        self.baseFeePerGas = try container.decodeHex(to: BigUInt.self, key: .baseFeePerGas)
+
+        // optional, since pre EIP-1559 block haven't such property.
+        self.baseFeePerGas = try? container.decodeHex(to: BigUInt.self, key: .baseFeePerGas)
 
         self.timestamp = try container.decodeHex(to: Date.self, key: .timestamp)
 
