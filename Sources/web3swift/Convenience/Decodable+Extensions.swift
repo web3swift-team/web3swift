77a1c7e6d118e5b48c2bf74a04007468a1b0bb94
Yaroslav <yaroslav.yashin@me.com>
2022-04-11 19:38:44 +0300

    Move AnyCodingKeys to appropriate place

--- Sources/web3swift/Convenience/Decodable+Extensions.swift
+++ Sources/web3swift/Convenience/Decodable+Extensions.swift
@@ -8,20 +8,6 @@
 import BigInt
 import Foundation
 
-struct AnyCodingKey: CodingKey {
-    var stringValue: String
-    var intValue: Int?
-
-    init?(stringValue: String) {
-        self.stringValue = stringValue
-    }
-
-    init?(intValue: Int) {
-        self.intValue = intValue
-        self.stringValue = String(intValue)
-    }
-}
-
 extension KeyedDecodingContainer {
     /// Decodes a value of the given key from Hex to `DecodableFromHex`
     ///
--- Sources/web3swift/Convenience/Encodable+Extensions.swift
+++ Sources/web3swift/Convenience/Encodable+Extensions.swift
@@ -7,6 +7,20 @@
 //
 import Foundation
 
+struct AnyCodingKey: CodingKey {
+    var stringValue: String
+    var intValue: Int?
+
+    init?(stringValue: String) {
+        self.stringValue = stringValue
+    }
+
+    init?(intValue: Int) {
+        self.intValue = intValue
+        self.stringValue = String(intValue)
+    }
+}
+
 extension KeyedEncodingContainer {
     /// Encodes the given value for the given key.
     ///
