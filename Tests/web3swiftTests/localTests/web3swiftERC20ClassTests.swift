27f504efde8d99ece6c7ffaebbafc82c8e801eb0
pharms.eth <100330083+pharms-eth@users.noreply.github.com>
2022-04-12 23:02:41 -0700

    propagate async

--- Tests/web3swiftTests/localTests/web3swiftERC20ClassTests.swift
+++ Tests/web3swiftTests/localTests/web3swiftERC20ClassTests.swift
@@ -11,23 +11,27 @@ import BigInt
 
 class web3swiftERC20ClassTests: XCTestCase {
     
-    func testERC20TokenCreation() throws {
-        let (web3, _, receipt, _) = try web3swiftHelpers.localDeployERC20()
+    func testERC20TokenCreation() async throws {
+        let (web3, _, receipt, _) = try await web3swiftHelpers.localDeployERC20()
         let erc20token = ERC20.init(web3: web3, provider: web3.provider, address: receipt.contractAddress!)
-        erc20token.readProperties()
-        XCTAssert(erc20token.symbol == "w3s")
-        XCTAssert(erc20token.name == "web3swift")
-        XCTAssert(erc20token.decimals == 18)
+        try await erc20token.readProperties()
+
+        let symbol = try await erc20token.symbol()
+        let name = try await erc20token.name()
+        let decimals = try await erc20token.decimals()
+        XCTAssert(symbol == "w3s")
+        XCTAssert(name == "web3swift")
+        XCTAssert(decimals == 18)
     }
     
-    func testERC20tokenBalanceAndAllowance() throws {
-        let (web3, _, receipt, _) = try web3swiftHelpers.localDeployERC20()
+    func testERC20tokenBalanceAndAllowance() async throws {
+        let (web3, _, receipt, _) = try await web3swiftHelpers.localDeployERC20()
         let erc20token = ERC20.init(web3: web3, provider: web3.provider, address: receipt.contractAddress!)
         
         let userAddress = EthereumAddress("0xe22b8979739D724343bd002F9f432F5990879901")!
         
-        let balance = try erc20token.getBalance(account: userAddress)
-        let allowance = try erc20token.getAllowance(originalOwner: userAddress, delegate: userAddress)
+        let balance = try await erc20token.getBalance(account: userAddress)
+        let allowance = try await erc20token.getAllowance(originalOwner: userAddress, delegate: userAddress)
         XCTAssert(String(balance) == "1024")
         XCTAssert(allowance == 0)
     }
