cd751ee776b84879818de1ac7a7787f67aa41e4d
Yaroslav <yaroslav.yashin@me.com>
2022-05-14 17:44:59 +0700

    `JSONRPCParam` enum implementation.
    Enum to compose request to the node params.
    
    In most cases request params are passed to Ethereum JSON RPC request as array of mixed type values, such as `[12,"this",true]`,
    thus this is not appropriate API design we have what we have.
    
    Meanwhile swift don't provide strict way to compose such array it gives some hacks to solve this task
    and one of them is using `RawRepresentable` protocol.
    
    Conforming this protocol gives designated type ability to represent itself in `String` representation in any way.
    
    So in our case we're using such to implement custom `encode` method to any used in node request params types.

--- Sources/web3swift/Web3/Web3+JSONRPC.swift
+++ Sources/web3swift/Web3/Web3+JSONRPC.swift
@@ -22,6 +22,7 @@ public struct Counter {
 }
 
 /// JSON RPC request structure for serialization and deserialization purposes.
+//public struct JSONRPCrequest<T: Encodable>: Encodable {
 public struct JSONRPCrequest: Encodable {
     public var jsonrpc: String = "2.0"
     public var method: JSONRPCmethod?
@@ -291,6 +292,8 @@ public struct JSONRPCparams: Encodable{
     public var params = [Any]()
 
     // FIXME: Make me generic with encodableToHex (watch out bool)
+    /// This method is encoding all possible params of a Ethereum JSON RPC request.
+    /// So type of this struct
     public func encode(to encoder: Encoder) throws {
         var container = encoder.unkeyedContainer()
         for par in params {
@@ -306,5 +309,141 @@ public struct JSONRPCparams: Encodable{
                 try container.encode(p)
             }
         }
+    }
+}
+
+/**
+ Enum to compose request to the node params.
+
+ In most cases request params are passed to Ethereum JSON RPC request as array of mixed type values, such as `[12,"this",true]`,
+ thus this is not appropriate API design we have what we have.
+
+ Meanwhile swift don't provide strict way to compose such array it gives some hacks to solve this task
+ and one of them is using `RawRepresentable` protocol.
+
+ Conforming this protocol gives designated type ability to represent itself in `String` representation in any way.
+
+ So in our case we're using such to implement custom `encode` method to any used in node request params types.
+
+ Latter is required to encode array of `JSONRPCParam` to not to `[JSONRPCParam.int(1)]`, but to `[1]`.
+
+ Here's an example of using this enum in field.
+ ```swift
+ let jsonRPCParams: [JSONRPCParam] = [
+    .init(rawValue: 12)!,
+    .init(rawValue: "this")!,
+    .init(rawValue: 12.2)!,
+    .init(rawValue: [12.2, 12.4])!
+ ]
+ let encoded = try JSONEncoder().encode(jsonRPCParams)
+ print(String(data: encoded, encoding: .utf8)!)
+ //> [12,\"this\",12.2,[12.2,12.4]]`
+ ```
+ */
+public enum JSONRPCParam {
+    case int(Int)
+    case intArray([Int])
+
+    case double(Double)
+    case doubleArray([Double])
+
+    case string(String)
+    case stringArray([String])
+
+    case bool(Bool)
+    case transaction(TransactionParameters)
+    case eventFilter(EventFilterParameters)
+}
+
+extension JSONRPCParam: RawRepresentable {
+    /**
+     This init required by `RawRepresentable` protocol, which is requred bo encode mixed type values array in JSON.
+
+     This protocol used to implement custom `encode` method for that enum,
+     which is encodes array of self into array of self assotiated values.
+
+     You're totally free to use explicit and more convenience member init as `JSONRPCParam.int(12)` in your code.
+     */
+    public init?(rawValue: Encodable) {
+        /// force casting in this switch is safe because
+        /// each `rawValue` forced to casts only in exact case which is runs based on `rawValues` type
+        // swiftlint:disable force_cast
+        switch type(of: rawValue) {
+        case is Int.Type: self = .int(rawValue as! Int)
+        case is [Int].Type: self = .intArray(rawValue as! [Int])
+
+        case is String.Type: self = .string(rawValue as! String)
+        case is [String].Type: self = .stringArray(rawValue as! [String])
+
+        case is Double.Type: self = .double(rawValue as! Double)
+        case is [Double].Type: self = .doubleArray(rawValue as! [Double])
+
+        case is Bool.Type: self = .bool(rawValue as! Bool)
+        case is TransactionParameters.Type: self = .transaction(rawValue as! TransactionParameters)
+        case is EventFilterParameters.Type: self = .eventFilter(rawValue as! EventFilterParameters)
+        default: return nil
+        }
+        // swiftlint:enable force_cast
+    }
+
+    /// Returning associated value of the enum case only.
+    public var rawValue: Encodable {
+        // cases can't be merged, coz it cause compiler error since it couldn't predict what exact type on exact case will be returned.
+        switch self {
+        case let .int(value): return value
+        case let .intArray(value): return value
+
+        case let .string(value): return value
+        case let .stringArray(value): return value
+
+        case let .double(value): return value
+        case let .doubleArray(value): return value
+
+        case let .bool(value): return value
+        case let .transaction(value): return value
+        case let .eventFilter(value): return value
+        }
+    }
+}
+
+extension JSONRPCParam: Encodable {
+    /**
+     This encoder encodes `JSONRPCParam` assotiated value ignoring self value
+
+     This is required to encode mixed types array, like
+
+     ```swift
+     let someArray: [JSONRPCParam] = [
+        .init(rawValue: 12)!,
+        .init(rawValue: "this")!,
+        .init(rawValue: 12.2)!,
+        .init(rawValue: [12.2, 12.4])!
+     ]
+     let encoded = try JSONEncoder().encode(someArray)
+     print(String(data: encoded, encoding: .utf8)!)
+     //> [12,\"this\",12.2,[12.2,12.4]]`
+     ```
+     */
+    public func encode(to encoder: Encoder) throws {
+        var enumContainer = encoder.singleValueContainer()
+        /// force casting in this switch is safe because
+        /// each `rawValue` forced to casts only in exact case which is runs based on `rawValues` type
+        // swiftlint:disable force_cast
+        switch type(of: self.rawValue) {
+        case is Int.Type: try enumContainer.encode(rawValue as! Int)
+        case is [Int].Type: try enumContainer.encode(rawValue as! [Int])
+
+        case is String.Type: try enumContainer.encode(rawValue as! String)
+        case is [String].Type: try enumContainer.encode(rawValue as! [String])
+
+        case is Double.Type: try enumContainer.encode(rawValue as! Double)
+        case is [Double].Type: try enumContainer.encode(rawValue as! [Double])
+
+        case is Bool.Type: try enumContainer.encode(rawValue as! Bool)
+        case is TransactionParameters.Type: try enumContainer.encode(rawValue as! TransactionParameters)
+        case is EventFilterParameters.Type: try enumContainer.encode(rawValue as! EventFilterParameters)
+        default: break
+        }
+        // swiftlint:enable force_cast
     }
 }
