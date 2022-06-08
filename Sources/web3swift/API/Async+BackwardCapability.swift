55732abd04fab9437b6431fe48876ae016971d18
Yaroslav <yaroslav.yashin@me.com>
2022-06-08 22:40:42 +0700

    Delete unused `URLSession.data` method method.

--- Sources/web3swift/API/Async+BackwardCapability.swift
+++ Sources/web3swift/API/Async+BackwardCapability.swift
@@ -10,19 +10,6 @@ import Foundation
 @available(iOS, obsoleted: 15.0, message: "Use the built-in API instead")
 @available(macOS, obsoleted: 12.0, message: "Use the built-in API instead")
 extension URLSession {
-    func data(fromUrl url: URL) async throws -> (Data, URLResponse) {
-        try await withCheckedThrowingContinuation { continuation in
-            let task = self.dataTask(with: url) { data, response, error in
-                guard let data = data, let response = response else {
-                    let error = error ?? URLError(.badServerResponse)
-                    return continuation.resume(throwing: error)
-                }
-                continuation.resume(returning: (data, response))
-            }
-            task.resume()
-        }
-    }
-
     func data(for request: URLRequest) async throws -> (Data, URLResponse) {
         var dataTask: URLSessionDataTask?
 
