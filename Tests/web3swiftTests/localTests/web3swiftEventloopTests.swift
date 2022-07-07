e07433d30f9e3ac7d48766161b27d98d503fc633
Jenea Vranceanu <jeneavranceanu@gmail.com>
2022-07-07 11:57:42 +0300

    chore: removed commented test - it was expecting a specific keystore in a JSON file to be attached to the project but there was none;

--- Tests/web3swiftTests/localTests/web3swiftEventloopTests.swift
+++ Tests/web3swiftTests/localTests/web3swiftEventloopTests.swift
@@ -33,40 +33,4 @@ class web3swiftEventloopTests: LocalTestCase {
 
         waitForExpectations(timeout: 60, handler: nil)
     }
-
-   // func testNonceMiddleware() throws {
-   //     let middleware = Web3.Utils.NonceMiddleware()
-   //     middleware.attach(ganache)
-
-   //     let sendToAddress = EthereumAddress("0xe22b8979739D724343bd002F9f432F5990879901")!
-   //     let ksData = getKeystoreData()
-        // FIXME: Ganache crash here
-   //     let tempKeystore = EthereumKeystoreV3(ksData!)
-   //     let keystoreManager = KeystoreManager([tempKeystore!])
-   //     ganache.addKeystoreManager(keystoreManager)
-
-   //     var tx = ganache.eth.sendETH(to: sendToAddress, amount: 1000)
-   //     tx!.transactionOptions.from = tempKeystore!.addresses!.first!
-   //     var result = try! tx!.send(password: "web3swift")
-   //     let newNonce = result.transaction.nonce
-   //     sleep(1)
-   //     let hookNewNonce = middleware.nonceLookups[tempKeystore!.addresses!.first!]!
-   //     XCTAssertEqual(newNonce, hookNewNonce)
-
-   //     tx = ganache.eth.sendETH(to: sendToAddress, amount: 1000)
-   //     tx!.transactionOptions.from = tempKeystore!.addresses!.first!
-   //     result = try! tx!.send(password: "web3swift")
-   //     sleep(1)
-   //     let newNonce2 = result.transaction.nonce
-   //     let hookNewNonce2 = middleware.nonceLookups[tempKeystore!.addresses!.first!]!
-   //     XCTAssert(newNonce2 == hookNewNonce2)
-   // }
-
-   func getKeystoreData() -> Data? {
-       let bundle = Bundle(for: type(of: self))
-       guard let path = bundle.path(forResource: "key", ofType: "json") else { return nil }
-       guard let data = NSData(contentsOfFile: path) else { return nil }
-       return data as Data
-   }
-
 }
