f96976029a42761a5f37838b74b2b59272e09779
Jenea Vranceanu <jeneavranceanu@gmail.com>
2022-07-11 11:25:31 +0300

    chore: moved EventFilter and all related classes, protocols and extensions to a separate file from ContractProtocol.swift

--- Sources/web3swift/Contract/EthereumFilterEncodingExtensions.swift
+++ Sources/web3swift/Contract/EthereumFilterEncodingExtensions.swift
@@ -7,33 +7,43 @@
 import Foundation
 import BigInt
 
-extension BigUInt: EventFilterEncodable {
+public protocol EventFilterComparable {
+    func isEqualTo(_ other: AnyObject) -> Bool
+}
+
+public protocol EventFilterEncodable {
+    func eventFilterEncoded() -> String?
+}
+
+public protocol EventFilterable: EventFilterComparable, EventFilterEncodable { }
+
+extension BigUInt: EventFilterable {
     public func eventFilterEncoded() -> String? {
         return self.abiEncode(bits: 256)?.toHexString().addHexPrefix()
     }
 }
 
-extension BigInt: EventFilterEncodable {
+extension BigInt: EventFilterable {
     public func eventFilterEncoded() -> String? {
         return self.abiEncode(bits: 256)?.toHexString().addHexPrefix()
     }
 }
 
-extension Data: EventFilterEncodable {
+extension Data: EventFilterable {
     public func eventFilterEncoded() -> String? {
         guard let padded = self.setLengthLeft(32) else {return nil}
         return padded.toHexString().addHexPrefix()
     }
 }
 
-extension EthereumAddress: EventFilterEncodable {
+extension EthereumAddress: EventFilterable {
     public func eventFilterEncoded() -> String? {
         guard let padded = self.addressData.setLengthLeft(32) else {return nil}
         return padded.toHexString().addHexPrefix()
     }
 }
 
-extension String: EventFilterEncodable {
+extension String: EventFilterable {
     public func eventFilterEncoded() -> String? {
         guard let data = self.data(using: .utf8) else {return nil}
         return data.sha3(.keccak256).toHexString().addHexPrefix()
