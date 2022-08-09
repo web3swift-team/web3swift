48590e5d0ae38beb6b234a5612202d87fd390e53
Jenea Vranceanu <jeneavranceanu@gmail.com>
2022-08-09 15:35:55 +0300

    fix: removed EventFilter duplicate

--- Sources/web3swift/Contract/EventFiltering.swift
+++ Sources/web3swift/Contract/EventFiltering.swift
@@ -11,65 +11,6 @@ public struct EventFilter {
     public enum Block {
         case latest
         case pending
-        case blockNumber(UInt)
-
-        var encoded: String {
-            switch self {
-            case .latest:
-                return "latest"
-            case .pending:
-                return "pending"
-            case .blockNumber(let number):
-                return String(number, radix: 16).addHexPrefix()
-            }
-        }
-    }
-
-    public init() { }
-
-    public init(fromBlock: Block?, toBlock: Block?,
-                addresses: [EthereumAddress]? = nil,
-                parameterFilters: [[EventFilterable]?]? = nil) {
-        self.fromBlock = fromBlock
-        self.toBlock = toBlock
-        self.addresses = addresses
-        self.parameterFilters = parameterFilters
-    }
-
-    public var fromBlock: Block?
-    public var toBlock: Block?
-    public var addresses: [EthereumAddress]?
-    public var parameterFilters: [[EventFilterable]?]?
-
-    public func rpcPreEncode() -> EventFilterParameters {
-        var encoding = EventFilterParameters()
-        if self.fromBlock != nil {
-            encoding.fromBlock = self.fromBlock!.encoded
-        }
-        if self.toBlock != nil {
-            encoding.toBlock = self.toBlock!.encoded
-        }
-        if self.addresses != nil {
-            if self.addresses!.count == 1 {
-                encoding.address = [self.addresses![0].address]
-            } else {
-                var encodedAddresses = [String?]()
-                for addr in self.addresses! {
-                    encodedAddresses.append(addr.address)
-                }
-                encoding.address = encodedAddresses
-            }
-        }
-        return encoding
-    }
-}
-
-// MARK: - Internal functions
-
-public struct EventFilter {
-    public enum Block {
-        case latest
-        case pending
         case blockNumber(UInt64)
 
         var encoded: String {
