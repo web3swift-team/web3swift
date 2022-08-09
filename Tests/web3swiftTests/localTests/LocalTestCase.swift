7c8b53c8934c99c36a90583f34768c40556f407e
Jenea Vranceanu <jeneavranceanu@gmail.com>
2022-08-09 21:40:51 +0300

    fix: LocalTestCase - pre-population of blockchain with 25 blocks must happen synchronously before test cases run. Fixed by using `func setUp() async throws` instead of `class func setUp()`.

--- Tests/web3swiftTests/localTests/LocalTestCase.swift
+++ Tests/web3swiftTests/localTests/LocalTestCase.swift
@@ -11,38 +11,28 @@ class LocalTestCase: XCTestCase {
 
     static let url = URL(string: "http://127.0.0.1:8545")!
 
-    static var isSetUp = false
+    override func setUp() async throws {
+        let web3 = try! await Web3.new(LocalTestCase.url)
 
-    override class func setUp() {
-        super.setUp()
+        let block = try! await web3.eth.blockNumber()
+        if block >= 25 { return }
 
-        Task {
-            // check to see if we need to run the one-time setup
-            if isSetUp { return }
-            isSetUp = true
+        print("\n ****** Preloading Ganache (\(25 - block) blocks) *****\n")
 
-            let web3 = try! await Web3.new(LocalTestCase.url)
+        let allAddresses = try! await web3.eth.ownedAccounts()
+        let sendToAddress = allAddresses[0]
+        let contract = web3.contract(Web3.Utils.coldWalletABI, at: sendToAddress, abiVersion: 2)
+        let value = Utilities.parseToBigUInt("1.0", units: .eth)
 
-            let block = try! await web3.eth.blockNumber()
-            if block >= 25 { return }
+        let from = allAddresses[0]
+        let writeTX = contract!.write("fallback")!
+        writeTX.transactionOptions.from = from
+        writeTX.transactionOptions.value = value
+        writeTX.transactionOptions.gasLimit = .manual(78423)
+        writeTX.transactionOptions.gasPrice = .manual(20000000000)
 
-            print("\n ****** Preloading Ganache (\(25 - block) blocks) *****\n")
-
-            let allAddresses = try! await web3.eth.ownedAccounts()
-            let sendToAddress = allAddresses[0]
-            let contract = web3.contract(Web3.Utils.coldWalletABI, at: sendToAddress, abiVersion: 2)
-            let value = Utilities.parseToBigUInt("1.0", units: .eth)
-
-            let from = allAddresses[0]
-            let writeTX = contract!.write("fallback")!
-            writeTX.transactionOptions.from = from
-            writeTX.transactionOptions.value = value
-            writeTX.transactionOptions.gasLimit = .manual(78423)
-            writeTX.transactionOptions.gasPrice = .manual(20000000000)
-
-            for _ in block..<25 {
-                let _ = try! await writeTX.send(password: "")
-            }
+        for _ in block..<25 {
+            let _ = try! await writeTX.send(password: "")
         }
     }
 }
