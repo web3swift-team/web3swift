119982b106a65b1b052bff2bc67e4a9aac7c6316
Yaroslav <yaroslav.yashin@me.com>
2022-09-23 00:06:45 +0700

    Update test description.

--- Tests/web3swiftTests/remoteTests/ST20AndSecurityTokenTests.swift
+++ Tests/web3swiftTests/remoteTests/ST20AndSecurityTokenTests.swift
@@ -19,11 +19,12 @@ class ST20AndSecurityTokenTests: XCTestCase {
         let web3 = await Web3.InfuraKovanWeb3(accessToken: Constants.infuraToken)
         let w3sTokenAddress = EthereumAddress("0x2dD33957C90880bE4Ee9fd5F703110BDA2E579EC")!
         let st20token = ST20.init(web3: web3, provider: web3.provider, address: w3sTokenAddress)
-        try await st20token.readProperties()
+//        try await st20token.readProperties()
         let symbol = try await st20token.symbol()
         let name = try await st20token.name()
         let decimals = try await st20token.decimals()
         // FIXME Reading sometimes messes values.
+        // XCTAssertEqual failed: ("Mimi") is not equal to ("MIMI")
         XCTAssertEqual(symbol, "MIMI")
         XCTAssertEqual(name, "Mimi")
         XCTAssertEqual(decimals, 18)
