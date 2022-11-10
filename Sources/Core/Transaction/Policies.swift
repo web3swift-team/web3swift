e4b73e9c39ee649600b254be1097676def4fab47
Jenea Vranceanu <36865532+JeneaVranceanu@users.noreply.github.com>
2022-11-10 22:54:51 +0200

    chore: added links to EIP-1559 for max fee and max priority fee per gas

--- Sources/Core/Transaction/Policies.swift
+++ Sources/Core/Transaction/Policies.swift
@@ -13,8 +13,8 @@ public typealias NoncePolicy = BlockNumber
 /// Policies for resolving values like:
 /// - gas required for transaction execution
 /// - gas price
-/// - maximum fee per gas
-/// - maximum priority fee per gas
+/// - maximum fee per gas (see [EIP-1559](https://eips.ethereum.org/EIPS/eip-1559))
+/// - maximum priority fee per gas (see [EIP-1559](https://eips.ethereum.org/EIPS/eip-1559))
 public enum ValueResolutionPolicy {
     /// What ever value will be resolved is the one to be applied
     case automatic
