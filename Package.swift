d135e9196e154a31ebfa313e5815ff083c252db5
Sven A. Schmidt <sas@finestructure.co>
2022-11-13 11:59:26 +0200

    Fix validation in SPI package list
    Our package list validation runs on Linux, which prevents this package from being added:

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ import PackageDescription
 let excludeFiles = [
     "./Browser/BrowserViewController.swift" // Because of inheriting iOS only class failed to build on macOS.
 ]
-#elseif os(iOS)
+#else
 let excludeFiles: String = []
 #endif
 
