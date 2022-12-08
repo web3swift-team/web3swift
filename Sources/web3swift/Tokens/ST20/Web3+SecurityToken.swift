39b141172e9609477a6a6ed10213322ef3c82dbd
Jenea Vranceanu <jeneavranceanu@gmail.com>
2022-12-08 19:06:36 +0200

    chore: removed empty array argument - empty array is the default value already

--- Sources/web3swift/Tokens/ST20/Web3+SecurityToken.swift
+++ Sources/web3swift/Tokens/ST20/Web3+SecurityToken.swift
@@ -91,7 +91,7 @@ public class SecurityToken: ISecurityToken, ERC20BaseProperties {
 
     func tokenDetails() async throws -> [UInt32] {
         transaction.callOnBlock = .latest
-        let result = try await contract.createReadOperation("tokenDetails", parameters: [] as [AnyObject])!.callContractMethod()
+        let result = try await contract.createReadOperation("tokenDetails")!.callContractMethod()
         guard let res = result["0"] as? [UInt32] else { throw Web3Error.processingError(desc: "Failed to get result of expected type from the Ethereum node") }
         return res
     }
