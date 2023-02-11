1c0f2f75973ad0854e29f18a6aa6fc02fe88ed3f
Jenea Vranceanu <jeneavranceanu@gmail.com>
2023-02-11 18:06:58 +0200

    fix: docs fix in Web3.Personal

--- Sources/web3swift/Web3/Web3+Personal.swift
+++ Sources/web3swift/Web3/Web3+Personal.swift
@@ -9,77 +9,48 @@ import Web3Core
 
 extension Web3.Personal {
 
-    /**
-     *Locally or remotely sign a message (arbitrary data) with the private key. To avoid potential signing of a transaction the message is first prepended by a special header and then hashed.*
-
-     - parameters:
-     - message: Message Data
-     - from: Use a private key that corresponds to this account
-     - password: Password for account if signing locally
-
-     - returns:
-     - Result object
-
-     - important: This call is synchronous
-
-     */
+    /// Locally or remotely sign a message (arbitrary data) with the private key.
+    /// To avoid potential signing of a transaction the message is first prepended by a special header and then hashed.
+    /// - Parameters:
+    ///   - message: raw message as bytes (e.g. UTF-8 bytes of a string);
+    ///   - from: an account whose private key will be used;
+    ///   - password: password to extract private key;
+    /// - Returns: signed personal message
     public func signPersonalMessage(message: Data, from: EthereumAddress, password: String) async throws -> Data {
-        let result = try await self.signPersonal(message: message, from: from, password: password)
+        let result = try await signPersonal(message: message, from: from, password: password)
         return result
     }
 
-    /**
-     *Unlock an account on the remote node to be able to send transactions and sign messages.*
-
-     - parameters:
-     - account: EthereumAddress of the account to unlock
-     - password: Password to use for the account
-     - seconds: Time interval before automatic account lock by Ethereum node
-
-     - returns:
-     - Result object
-
-     - important: This call is synchronous. Does nothing if private keys are stored locally.
-
-     */
+    /// Unlock an account on the remote node to be able to send transactions and sign messages.
+    /// - Parameters:
+    ///   - account: EthereumAddress of the account to unlock
+    ///   - password: Password for the account
+    ///   - seconds: Time interval before automatic account lock by Ethereum node. Default 300
+    /// - Returns: `true` if account was unlocked.
     public func unlockAccount(account: EthereumAddress, password: String, seconds: UInt = 300) async throws -> Bool {
-        let result = try await self.unlock(account: account, password: password)
+        let result = try await unlock(account: account, password: password)
         return result
     }
 
-    /**
-     *Recovers a signer of some message. Message is first prepended by special prefix (check the "signPersonalMessage" method description) and then hashed.*
-
-     - parameters:
-     - personalMessage: Message Data
-     - signature: Serialized signature, 65 bytes
-
-     - returns:
-     - Result object
-
-     */
-    public func ecrecover(personalMessage: Data, signature: Data) throws -> EthereumAddress {
-        guard let recovered = Utilities.personalECRecover(personalMessage, signature: signature) else {
-            throw Web3Error.dataError
-        }
-        return recovered
+    /// Recovers a signer of some personal message. Message will be first prepended by special prefix
+    /// (check the "signPersonalMessage" method description) and then hashed before the recovery attempt.
+    ///
+    /// If you have a hash instead of a message use ``Web3/Personal/ecrecover(hash:signature:)``
+    ///
+    /// - Parameters:
+    ///   - personalMessage: raw message as bytes (e.g. UTF-8 bytes of a string);
+    ///   - signature: signature that is the result of signing the `personalMessage`;
+    /// - Returns: address of the signer or `nil`.
+    public func ecrecover(personalMessage: Data, signature: Data) -> EthereumAddress? {
+        Utilities.personalECRecover(personalMessage, signature: signature)
     }
 
-    /**
-     *Recovers a signer of some hash. Checking what is under this hash is on behalf of the user.*
-
-     - parameters:
-     - hash: Signed hash
-     - signature: Serialized signature, 65 bytes
-
-     - returns:
-     - Result object
-
-     */
-    public func ecrecover(hash: Data, signature: Data) throws -> EthereumAddress {
-        guard let recovered = Utilities.hashECRecover(hash: hash, signature: signature) else {
-            throw Web3Error.dataError
-        }
-        return recovered
+    /// Recovers a signer of some hash.
+    /// - Parameters:
+    ///   - hash: some hash, e.g. hashed personal message;
+    ///   - signature: 65 bytes serialized signature;
+    /// - Returns: address of the signer or `nil`.
+    public func ecrecover(hash: Data, signature: Data) -> EthereumAddress {
+        Utilities.hashECRecover(hash: hash, signature: signature)
     }
 }
