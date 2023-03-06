0847410c2ade515f7771660c42252a837ce316a7
pharms.eth <100330083+pharms-eth@users.noreply.github.com>
2023-03-05 20:36:22 -0800

    Update String+Extension.swift

--- Sources/Web3Core/Utility/String+Extension.swift
+++ Sources/Web3Core/Utility/String+Extension.swift
@@ -32,26 +32,6 @@ extension String {
         return output
     }
 
-    /// Splits a string into groups of `every` n characters, grouping from left-to-right by default. If `backwards` is true, right-to-left.
-   public func split(every: Int, backwards: Bool = false) -> [String] {
-       var result = [String]()
-
-       for i in stride(from: 0, to: self.count, by: every) {
-           switch backwards {
-           case true:
-               let endIndex = self.index(self.endIndex, offsetBy: -i)
-               let startIndex = self.index(endIndex, offsetBy: -every, limitedBy: self.startIndex) ?? self.startIndex
-               result.insert(String(self[startIndex..<endIndex]), at: 0)
-           case false:
-               let startIndex = self.index(self.startIndex, offsetBy: i)
-               let endIndex = self.index(startIndex, offsetBy: every, limitedBy: self.endIndex) ?? self.endIndex
-               result.append(String(self[startIndex..<endIndex]))
-           }
-       }
-
-       return result
-   }
-
     public subscript (bounds: CountableClosedRange<Int>) -> String {
         let start = index(self.startIndex, offsetBy: bounds.lowerBound)
         let end = index(self.startIndex, offsetBy: bounds.upperBound)
