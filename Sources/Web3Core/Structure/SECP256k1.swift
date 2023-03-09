5b0dba3dd116462d256b72ea0b94e434c5504c73
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2023-03-09 18:36:11 +0000

    address review
    Signed-off-by: Sara Tavares <saratavares843@gmail.com>

--- Sources/Web3Core/Structure/SECP256k1.swift
+++ Sources/Web3Core/Structure/SECP256k1.swift
@@ -215,7 +215,7 @@ extension SECP256K1 {
                 let setSignaturePointer = setSignatureRawPointer.assumingMemoryBound(to: UInt8.self)
                 return withUnsafePointer(to: &recoverableSignature) { (signaturePointer: UnsafePointer<secp256k1_ecdsa_recoverable_signature>) -> Int32 in
                     withUnsafeMutablePointer(to: &v) { (vPtr: UnsafeMutablePointer<Int32>) -> Int32 in
-                        let res = secp256k1_ecdsa_recoverable_signature_serialize_compact(context, setSignaturePointer, vPtr, signaturePointer)
+                        let res = secp256k1_ecdsa_recoverable_signature_serialize_compact(context, signaturePointer, vPtr, signaturePointer)
                         return res
                     }
                 }
