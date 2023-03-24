f909a0085279c83e02577172f4da09931e758238
Jenea Vranceanu <36865532+JeneaVranceanu@users.noreply.github.com>
2023-03-24 11:43:30 +0200

    chore: docs instead of a comment

--- Tests/web3swiftTests/localTests/BIP39Tests.swift
+++ Tests/web3swiftTests/localTests/BIP39Tests.swift
@@ -31,7 +31,7 @@ final class BIP39Tests: XCTestCase {
         XCTAssert(seed == recoveredSeed)
     }
 
-//     https://github.com/trezor/python-mnemonic/blob/master/vectors.json
+    /// Test cases were borrowed from https://github.com/trezor/python-mnemonic/blob/master/vectors.json
     func testBIP39MnemonicIsMultipleOfThree() {
         // https://github.com/trezor/python-mnemonic/blob/master/vectors.json#L95
         let mnemonic_12 = "scheme spot photo card baby mountain device kick cradle pact join borrow"
@@ -80,7 +80,7 @@ final class BIP39Tests: XCTestCase {
         let keystore2 = try BIP32Keystore(mnemonics: mnemonic, password: "", mnemonicsPassword: "")
         XCTAssert(keystore1?.addresses?.first == keystore2?.addresses?.first)
     }
-    //====================================================================
+
     func testBIP39Array () throws {
         var entropy = Data.fromHex("00000000000000000000000000000000")!
         var phrase = BIP39.generateMnemonicsFrom(entropy: entropy)
