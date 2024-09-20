9f2f2e0ee4da088b145b24643a5c3e41cb88dcab
Parth <parth@codamusic.net>
2024-09-20 11:08:57 +0530

    - fix xcode 16 issue

--- Sources/secp256k1/scalar_4x64_impl.h
+++ Sources/secp256k1/scalar_4x64_impl.h
@@ -3,6 +3,7 @@
  * Distributed under the MIT software license, see the accompanying   *
  * file COPYING or http://www.opensource.org/licenses/mit-license.php.*
  **********************************************************************/
+#include <sys/types.h>
 
 #ifndef SECP256K1_SCALAR_REPR_IMPL_H
 #define SECP256K1_SCALAR_REPR_IMPL_H
