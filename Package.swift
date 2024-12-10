4bca11f8ac06ee787fa2490c832be839869c397e
Yaroslav Yashin <yaroslav.yashin@gmail.com>
2024-12-10 18:42:42 +0100

    tests

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "web3swift", targets: ["web3swift"])
     ],
     dependencies: [
-        .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
+        .package(url: "https://github.com/attaswift/BigInt.git", .upToNextMinor(from: "5.4.0")),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.5.1"),
         .package(name: "secp256k1", url: "https://github.com/GigaBitcoin/secp256k1.swift", .upToNextMinor(from: "0.10.0")),
     ],
